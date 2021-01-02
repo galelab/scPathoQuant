0324d8cecdd75fda69f5931e4fc40b310abee78c
Leanne <leanne382@gmail.com>
2021-01-01 20:15:21 -0800

    fix bug in viz.py

--- visualization/viz.py
+++ visualization/viz.py
@@ -8,10 +8,13 @@ import seaborn as sns
 
 def generate_viral_copy_plots(args, gene_name, dfumi):
     print ("STATUS: Generating violin plots for viral copies")
-    sns.set_theme(style="whitegrid")
-    dfumi[gene_name]=[gene_name]*dfumi.shape[0]
-    ax = sns.violinplot(x=gene_name, y="umi",data=dfumi)
-    ax.set(xlabel='', ylabel="counts")
-    g = ax.get_figure()
-    g.savefig(os.path.join(args.output_path, "viral_copy.png"), dpi=500)
+    if dfumi.shape[0] > 0:
+        sns.set_theme(style="whitegrid")
+        dfumi[gene_name]=[gene_name]*dfumi.shape[0]
+        ax = sns.violinplot(x=gene_name, y="umi",data=dfumi)
+        ax.set(xlabel='', ylabel="counts")
+        g = ax.get_figure()
+        g.savefig(os.path.join(args.output_path, "viral_copy.png"), dpi=500)
+    else:
+        print ("STATUS: No cells have viral reads not generating violin plot")
 
