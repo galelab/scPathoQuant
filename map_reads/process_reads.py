a91264063997acc190ecf3284f6dd4423e4191d0
Leanne <leanne382@gmail.com>
2021-01-01 17:08:02 -0800

    fix bugs in process_reads.py

--- map_reads/process_reads.py
+++ map_reads/process_reads.py
@@ -15,19 +15,27 @@ def _check_subprocess_run(returncode, stderrdata, runinfo):
 
 def process_unmapped_reads(args, samtoolspath, viable_cb):
     try: 
-        os.mkdir(args.output_path+"_tmp")
+       os.mkdir(args.output_path)
+    except:
+        pass 
+    try: 
+        os.mkdir(os.path.join(args.output_path, "_tmp"))
     except:
         pass
     # -- pull out unmapped reads
-    args=[samtoolspath+"samtools", "view", "-@", args.processors, "-b", "-h", "-f", "4", os.path.join(args.path10x, "outs", "possorted_genome_bam.bam"), "-o",  os.path.join(args.output_path, "_tmp","unmmapped.bam")]
-    process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE) 
-    print("STATUS: Extracting unmapped reads..")
+    arg=[samtoolspath+"samtools", "view", "-@", args.processors,
+        "-b", "-h", "-f", "4", os.path.join(args.path10x, "outs", "possorted_genome_bam.bam"),
+        "-o",  os.path.join(args.output_path, "_tmp","unmmapped.bam")]
+    process = subprocess.Popen(arg, stdout=subprocess.PIPE, stderr=subprocess.PIPE) 
+    # print("STATUS: Extracting unmapped reads..")
     stdoutdata, stderrdata = process.communicate()
     _check_subprocess_run(process.returncode, stderrdata, "extracting unmapped")
     
     # -- convert unmapped bam to sam file
-    args=[samtoolspath+"samtools","view" "-@", args.processors, "-h", os.path.join(args.output_path,"_tmp", "unmapped.bam"), "-o", os.path.join(args.output_path, "_tmp", "unmapped.sam")]
-    process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE) 
+    arg=[samtoolspath+"samtools","view", "-@", args.processors, 
+        "-h", os.path.join(args.output_path,"_tmp", "unmapped.bam"),
+        "-o", os.path.join(args.output_path, "_tmp", "unmapped.sam")]
+    process = subprocess.Popen(arg, stdout=subprocess.PIPE, stderr=subprocess.PIPE) 
     print("STATUS: Convert bam to sam for unmmaped reads..")
     stdoutdata, stderrdata = process.communicate()
     _check_subprocess_run(process.returncode, stderrdata, "convert bam to sam")
@@ -54,9 +62,9 @@ def process_unmapped_reads(args, samtoolspath, viable_cb):
                         fout.write(re.sub("CB:Z:", "", cellbarcode)+","+re.sub("UB:Z:", "", umi)+","+larray[0]+"\n")
 
     # -- convert to fastq file
-    args = [samtoolspath+"samtools", "bam2fq", "-@", args.processors, "-n","-O", "-s", os.path.join(args.output_path, "_tmp", "ummapped.fq"), os.path.join(args.output_path,"_tmp","unmmapped.bam")]
+    arg = [samtoolspath+"samtools", "bam2fq", "-@", args.processors, "-n","-O", "-s", os.path.join(args.output_path, "_tmp", "ummapped.fq"), os.path.join(args.output_path,"_tmp","unmmapped.bam")]
     f = open(args.output_path+"_tmp"+"ummapped.fq" "w") 
-    process = subprocess.Popen(args, stdout=f, stderr=subprocess.PIPE)
+    process = subprocess.Popen(arg, stdout=f, stderr=subprocess.PIPE)
     stdoutdata, stderrdata = process.communicate()
     f.close()
     _check_subprocess_run(process.returncode, stderrdata, "converting reads to fastq")
