b22bd76c022f34600b220747be2edc3ec7a1c04a
Leanne <leanne382@gmail.com>
2021-01-08 10:58:50 -0800

    fix bug in dealing with samples where no reads map to viral genes

--- scqv/quantify/viral_genes.py
+++ scqv/quantify/viral_genes.py
@@ -23,30 +23,39 @@ def quantify_reads(output_path,filename):
                     reads_mapping[larray[0]]=re.sub("XF:Z:", "", larray[-1])
 
     ## -- get umi and cell barcode information 
-    tmpdf = pd.DataFrame.from_dict(reads_mapping,orient='index')   
-    df = pd.read_csv(os.path.join(output_path,"_tmp", "barcode_umi_read_table.csv"))
-    df1 = df[df['read'].isin(reads_mapping.keys())]
-    df1 = df1.set_index('read')
-    df1 = pd.concat([df1, tmpdf],  axis=1, join="inner")
-    df1 = df1.reset_index() 
-    df1 = df1.rename(columns={"index":"read", 0: "gene"})
-    df2 = df1.groupby(['cell_barcode', 'umi', 'gene']).agg({'gene': lambda x: ','.join(x)})
-    df2.columns = ['combined_genes']
-    df2 = df2.reset_index()
-    drop_reads = []
-    for i in range(0, len(df2.combined_genes)): 
-        x = len(set(df2.combined_genes[i].split(','))) 
-        if x > 1:
-            drop_reads.append(i)
-    if len(drop_reads) > 0:
-        print ("STATUS: Dropping "+str(len(drop_reads))+" UMIs because of inconsistent gene annotations")
-        df2 = df2.drop(drop_reads)
+    if len(reads_mapping.keys()) > 0:
+        tmpdf = pd.DataFrame.from_dict(reads_mapping,orient='index')   
+        df = pd.read_csv(os.path.join(output_path,"_tmp", "barcode_umi_read_table.csv"))
+        df1 = df[df['read'].isin(reads_mapping.keys())]
+        df1 = df1.set_index('read')
+        df1 = pd.concat([df1, tmpdf],  axis=1, join="inner")
+        df1 = df1.reset_index() 
+        print (df1)
+        df1 = df1.rename(columns={"index":"read", 0: "gene"})
+        print (df1)
+        df2 = df1.groupby(['cell_barcode', 'umi', 'gene']).agg({'gene': lambda x: ','.join(x)})
+        df2.columns = ['combined_genes']
+        df2 = df2.reset_index()
+        drop_reads = []
+        for i in range(0, len(df2.combined_genes)): 
+            x = len(set(df2.combined_genes[i].split(','))) 
+            if x > 1:
+                drop_reads.append(i)
+        if len(drop_reads) > 0:
+            print ("STATUS: Dropping "+str(len(drop_reads))+" UMIs because of inconsistent gene annotations")
+            df2 = df2.drop(drop_reads)
 
-    df_umi = df2[["cell_barcode", "umi", "gene"]].groupby(["cell_barcode", "gene"]).count()
-    df_umi = df_umi.reset_index()
-    df_umi = df_umi.set_index('cell_barcode')
-    df_umi.to_csv(os.path.join(output_path, "virus_al_gene_counts.csv"))
-    return(df_umi)
+        df_umi = df2[["cell_barcode", "umi", "gene"]].groupby(["cell_barcode", "gene"]).count()
+        df_umi = df_umi.reset_index()
+        df_umi = df_umi.set_index('cell_barcode')
+        df_umi.to_csv(os.path.join(output_path, "virus_al_gene_counts.csv"))
+        return(df_umi)
+    else: 
+        print("STATUS: no reads mapping to viral genes.. will not add genes to 10x filtered files")
+        df_umi = pd.DataFrame(columns=["cell_barcode", "gene", "umi"])
+        df_umi = df_umi.set_index('cell_barcode')
+        df_umi.to_csv(os.path.join(output_path, "virus_al_gene_counts.csv"))
+        return(False)
 
 def htseq_run(args):
     ## -- generate gtf for viral genes
