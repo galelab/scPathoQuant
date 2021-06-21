fa94d1331cded8c01cb800eee5070e47cbafc24e
lwhitmore <leanne382@gmail.com>
2021-06-21 08:19:06 -0700

    add new alignment option parameter and fix os.path.join commands in map_reads.py

--- scqv/map_reads/map_reads.py
+++ scqv/map_reads/map_reads.py
@@ -13,10 +13,10 @@ PATH = os.path.dirname(os.path.abspath(__file__))
 PATH = re.sub("map_reads", "", PATH)
 if platform == "linux":
     bbmap2path = os.path.join(PATH, "aligntools", "bbmapv38.9")
-    bowtie2path = os.path.join(PATH, "aligntools/bowtie2-2.4.2-linux-x86_64/")
-    samtoolspath = os.path.join(PATH, "extra_tools/samtoolsv1.11_linux/bin/")
+    bowtie2path = os.path.join(PATH, "aligntools","bowtie2-2.4.2-linux-x86_64")
+    samtoolspath = os.path.join(PATH, "extra_tools", "samtoolsv1.11_linux","bin")
 elif platform == "OS":
-    bowtie2path = os.path.join(PATH, "aligntools/bowtie2-2.4.2-macos-x86_64/")
+    bowtie2path = os.path.join(PATH, "aligntools", "bowtie2-2.4.2-macos-x86_64")
 else:
     ValueError("Program wont run on this operating system "+platform)
 
@@ -42,15 +42,26 @@ def map2viralgenome(args):
         raise ValueError(args.aligner+" not an available aligner specify bbmap or bowtie2")
 
     # -- align reads 
-    if args.aligner == "bowtie2":
-        arg=[bowtie2path+"bowtie2", "-p", args.processors, "-q", os.path.join(args.output_path,"_tmp", "unmapped.fq"), "--local", "-x", os.path.join(args.path2genome,"genome"), "-S", os.path.join(args.output_path, "virus_al.sam")]
-        ef._run_subprocesses(arg, "STATUS: Align reads bowtie2 ...", "aligning reads")
-    elif arg.aligner == "bbmap":
-        arg=[bbmap2path+"bbmap.sh", "ref="+bbmapgenomefile, "in="+os.path.join(args.output_path,"_tmp", "unmapped.fq"), "nodisk=t" ,"local=t",
-            "covstats="+os.path.join(args.output_path, "constats.txt"), "covhist="+os.path.join(args.output_path,"covhist.txt"),
-            "basecov=+"+os.path.join(args.output_path,"basecov.txt"), "bincov="++os.path.join(args.output_path, "bincov.txt"),
-            "out="+os.path.join(args.output_path, "virus_al.sam")]
-        ef._run_subprocesses(arg, "STATUS: Align reads bbmap ...", "aligning reads")
+    if args.alignment_type == "local":
+        if args.aligner == "bowtie2":
+            arg=[os.path.join(bowtie2path, "bowtie2"), "-p", args.processors, "-q", os.path.join(args.output_path,"_tmp", "unmapped.fq"), "--local", "-x", os.path.join(args.path2genome,"genome"), "-S", os.path.join(args.output_path, "virus_al.sam")]
+            ef._run_subprocesses(arg, "STATUS: Align reads bowtie2 local...", "aligning reads")
+        elif arg.aligner == "bbmap":
+            arg=[os.path.join(bbmap2path, "bbmap.sh"), "ref="+bbmapgenomefile, "in="+os.path.join(args.output_path,"_tmp", "unmapped.fq"), "nodisk=t" ,"local=t",
+                "covstats="+os.path.join(args.output_path, "constats.txt"), "covhist="+os.path.join(args.output_path,"covhist.txt"),
+                "basecov=+"+os.path.join(args.output_path,"basecov.txt"), "bincov="+os.path.join(args.output_path, "bincov.txt"),
+                "out="+os.path.join(args.output_path, "virus_al.sam")]
+            ef._run_subprocesses(arg, "STATUS: Align reads bbmap local...", "aligning reads")
+    else:
+        if args.aligner == "bowtie2":
+            arg=[os.path.join(bowtie2path, "bowtie2"), "-p", args.processors, "-q", os.path.join(args.output_path,"_tmp", "unmapped.fq"), "-x", os.path.join(args.path2genome,"genome"), "-S", os.path.join(args.output_path, "virus_al.sam")]
+            ef._run_subprocesses(arg, "STATUS: Align reads bowtie2 global...", "aligning reads")
+        elif arg.aligner == "bbmap":
+            arg=[os.path.join(bbmap2path, "bbmap.sh"), "ref="+bbmapgenomefile, "in="+os.path.join(args.output_path,"_tmp", "unmapped.fq"), "nodisk=t" ,"local=f",
+                "covstats="+os.path.join(args.output_path, "constats.txt"), "covhist="+os.path.join(args.output_path,"covhist.txt"),
+                "basecov=+"+os.path.join(args.output_path,"basecov.txt"), "bincov="+os.path.join(args.output_path, "bincov.txt"),
+                "out="+os.path.join(args.output_path, "virus_al.sam")]
+            ef._run_subprocesses(arg, "STATUS: Align reads bbmap global...", "aligning reads")
 
     # --generate necessary output files
     arg=[samtoolspath+"samtools", "view", "-@", args.processors, "-F", "4", "-Sb", os.path.join(args.output_path,"virus_al.sam"), "-o", os.path.join(args.output_path, "virus_al.bam")]
