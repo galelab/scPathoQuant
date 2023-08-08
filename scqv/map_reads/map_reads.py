3ed08ebcbb0ed43e332a2210aaa821f05743e9b1
lwhitmore <leanne382@gmail.com>
2023-08-08 09:04:01 -0700

    convert over to pysam instead of locally installed samtools

--- scqv/map_reads/map_reads.py
+++ scqv/map_reads/map_reads.py
@@ -8,6 +8,7 @@ import glob
 import subprocess
 import scqv.extra_functions as ef
 from sys import platform
+import pysam
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 PATH = re.sub("map_reads", "", PATH)
@@ -17,7 +18,7 @@ if platform == "linux":
     # app_path = os.path.join(PATH, 'align', 'STAR')
     # os.environ["PATH"] += os.pathsep + app_path
     bowtie2path = os.path.join(PATH, "aligntools","bowtie2-2.4.2-linux-x86_64")
-    samtoolspath = os.path.join(PATH, "extra_tools", "samtoolsv1.11_linux","bin")
+    # samtoolspath = os.path.join(PATH, "extra_tools", "samtoolsv1.11_linux","bin")s
 elif platform == "OS":
     bowtie2path = os.path.join(PATH, "aligntools", "bowtie2-2.4.2-macos-x86_64")
     # star2path = os.path.join(PATH, "aligntools", "STAR-2.7.10a", "bin", "MacOSX_x86_64")
@@ -99,16 +100,25 @@ def map2viralgenome(args):
         # os.rename(os.path.join(args.output_path, "virus_alAligned.out.sam"), os.path.join(args.output_path, "virus_al.sam"))
 
     # --generate necessary output files
-    arg=[os.path.join(samtoolspath, "samtools"), "view", "-@", args.processors, "-F", "4", "-Sb", os.path.join(args.output_path,"virus_al.sam"), "-o", os.path.join(args.output_path, "virus_al.bam")]
-    ef._run_subprocesses(arg, "STATUS: Extracting mapped reads and converting to bam", "extracting mapped reads and converting to bam")
+    print("STATUS: Extracting mapped reads and converting to bam")
+    pysam.view("-@", str(args.processors),"-S", "-b", os.path.join(args.output_path,"virus_al.sam"), "-o", os.path.join(args.output_path, "virus_al.bam"), catch_stdout=False)
+    # arg=[os.path.join(samtoolspath, "samtools"), "view", "-@", args.processors, "-F", "4", "-Sb", os.path.join(args.output_path,"virus_al.sam"), "-o", os.path.join(args.output_path, "virus_al.bam")]
+    # ef._run_subprocesses(arg, "STATUS: Extracting mapped reads and converting to bam", "extracting mapped reads and converting to bam")
 
-    arg=[os.path.join(samtoolspath, "samtools"), "view", "-@", args.processors, "-h", os.path.join(args.output_path, "virus_al.bam"), "-o", os.path.join(args.output_path, "virus_al_mapped.sam")]
-    ef._run_subprocesses(arg, "STATUS: Converting mapped reads to from bam to sam file", "Converting mapped reads to from bam to sam file")
+    print("STATUS: Converting mapped reads to from bam to sam file")
+    pysam.view("-@", str(args.processors), "-h", os.path.join(args.output_path, "virus_al.bam"), "-o",  os.path.join(args.output_path, "virus_al_mapped.sam"), catch_stdout=False)
 
-    arg=[os.path.join(samtoolspath, "samtools"), "sort", os.path.join(args.output_path,"virus_al.bam"), "-o", os.path.join(args.output_path,"virus_al_sort.bam")]
-    ef._run_subprocesses(arg, "STATUS: Sorting bam", "sorting bam")
+    # arg=[os.path.join(samtoolspath, "samtools"), "view", "-@", args.processors, "-h", os.path.join(args.output_path, "virus_al.bam"), "-o", os.path.join(args.output_path, "virus_al_mapped.sam")]
+    # ef._run_subprocesses(arg, "STATUS: Converting mapped reads to from bam to sam file", "Converting mapped reads to from bam to sam file")
+    print("STATUS: Sorting bam")
+    pysam.sort("-@", str(args.processors), os.path.join(args.output_path,"virus_al.bam"), "-o",  os.path.join(args.output_path,"virus_al_sort.bam"))
+    # arg=[os.path.join(samtoolspath, "samtools"), "sort", os.path.join(args.output_path,"virus_al.bam"), "-o", os.path.join(args.output_path,"virus_al_sort.bam")]
+    # ef._run_subprocesses(arg, "STATUS: Sorting bam", "sorting bam")
 
-    arg=[os.path.join(samtoolspath, "samtools"), "index", os.path.join(args.output_path,"virus_al_sort.bam"), os.path.join(args.output_path,"virus_al_sort.bam.bai")]
-    ef._run_subprocesses(arg, "STATUS: generating bam index", "generating bam index")
+    print( "STATUS: generating bam index")
+    pysam.index("-@", str(args.processors), os.path.join(args.output_path,"virus_al_sort.bam"))
+
+    # arg=[os.path.join(samtoolspath, "samtools"), "index", os.path.join(args.output_path,"virus_al_sort.bam"), os.path.join(args.output_path,"virus_al_sort.bam.bai")]
+    # ef._run_subprocesses(arg, "STATUS: generating bam index", "generating bam index")
 
 
